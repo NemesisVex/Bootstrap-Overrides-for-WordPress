0cb68eebb8f3fbabe248752d6cd162531c6b4b54
Greg Bueno <greg@gregbueno.com>
2014-11-12 17:19:07 -0800

    Adding mu-plugins config file.

--- bootstrap-overrides.php
+++ bootstrap-overrides.php
@@ -4,7 +4,7 @@
   Plugin URI: https://bitbucket.org/NemesisVex/bootstrap-overrides-for-wordpress
   Description: This plugin overrides a number of functions in wp-includes to accommodate Bootstrap classes. Of course, any changes to these functions in Core will not be reflected.
   Author: Greg Bueno
-  Version: 1.0.1
+  Version: 1.0.3
   Author URI: http://vigilantmedia.com/
  */
 
